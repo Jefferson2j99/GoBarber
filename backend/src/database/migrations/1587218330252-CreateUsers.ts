014998c6075589c36bbc8151fd8ddfcbb047538f
Jefferson Oliveira <jefferson2112@hotmail.com>
2020-04-18 11:54:23 -0300

    Model e migration de usuário

--- backend/src/database/migrations/1587218330252-CreateUsers.ts
+++ backend/src/database/migrations/1587218330252-CreateUsers.ts
@@ -1,10 +1,10 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export default class CreateAppointments1587169184115 implements MigrationInterface {
+export default class CreateUsers1587218330252 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -14,14 +14,17 @@ export default class CreateAppointments1587169184115 implements MigrationInterfa
                         default: 'uuid_generate_v4()'
                     },
                     {
-                        name: 'provider',
-                        type: 'varchar',
-                        isNullable: false
+                        name: 'name',
+                        type: 'varchar'
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
-                        isNullable: false
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar'
                     },
                     {
                         name: 'created_at',
@@ -39,7 +42,7 @@ export default class CreateAppointments1587169184115 implements MigrationInterfa
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 
 }
