74e6a5f96772a4a3f0e2e7066337ab03e3ab58e1
Nelio Alves <jefferson2112@hotmail.com>
2020-05-05 17:03:39 -0300

    Validando cadastro

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,10 +10,24 @@ import Button from '../../components/button';
 
 import { Container, Content, Background } from './styles';
 
-const Signup: React.FC = () => {
-  function handleSubmmite(data: object): void {
-    console.log(data);
-  }
+const SignUp: React.FC = () => {
+  const handleSubmmite = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
@@ -45,4 +60,4 @@ const Signup: React.FC = () => {
   );
 };
 
-export default Signup;
+export default SignUp;
