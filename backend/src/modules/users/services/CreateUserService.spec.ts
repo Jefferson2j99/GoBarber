e80b0e6831fc1aa745ed890fb9b231c3b2b6459b
Nelio Alves <jefferson2112@hotmail.com>
2020-06-29 19:11:44 -0300

    Testes de autenticação

--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -1,12 +1,18 @@
 import AppError from '@shared/errors/AppError';
 
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const creteUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const creteUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await creteUser.execute({
       name: 'John Doe',
@@ -19,16 +25,21 @@ describe('CreateUser', () => {
 
   it('should be not able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const creteUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
 
-    const user = await creteUser.execute({
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@gmail.com',
       password: '123345',
     });
 
     expect(
-      creteUser.execute({
+      createUser.execute({
         name: 'John Doe',
         email: 'johndoe@gmail.com',
         password: '123345',
